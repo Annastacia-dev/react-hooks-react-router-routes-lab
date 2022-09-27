2450e45c54c94827c8fe10d819f8f2eb5c810f37
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2022-09-27 16:22:40 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Actors Page</h1>
+    {actors.map(actor => {
+      return (
+        <div key={Math.floor(Math.random() * 10000)}>
+          <h2>Name: {actor.name}</h2>
+          <p>Movies:</p>
+          <ul>
+            {actor.movies.map(movie => {
+              return (
+                <li key={Math.floor(Math.random() * 10000)}>{movie}</li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    })}
+    {/*{code here}*/}</div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Directors Page</h1>
+    {directors.map(director => {
+      return (
+        <div key={Math.floor(Math.random() * 10000)}>
+          <h2>Name: {director.name}</h2>
+          <p>Movies:</p>
+          <ul>
+            {director.movies.map(movie => {
+              return (
+                <li key={Math.floor(Math.random() * 10000)}>{movie}</li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    })}
+    {/*{code here}*/}</div>;
 }
 
 export default Directors;
